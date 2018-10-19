393b959ffb9809fe02baf9201473b2653aca9a00
20171105131 <1105197998@qq.com>
2018-10-19 08:29:44 +0800

    增加 减 乘 除

---
+++ ViewController.swift
@@ -0,0 +1,137 @@
+//
+//  ViewController.swift
+//  jisuanqi
+//
+//  Created by 唐伟强 on 2018/9/27.
+//  Copyright © 2018年 唐伟强. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    var temp = 0
+    var flag = 0
+
+    @IBOutlet weak var caculaterDisplay: UITextField!
+    
+    
+    @IBAction func buttonCaculater(_ sender: Any)
+    {
+        var sum = 0
+        sum = temp +
+            Int(caculaterDisplay.text!)!
+        caculaterDisplay.text = "\(sum)"
+    }
+    
+    
+    @IBAction func buttenClear(_ sender: Any)
+    {
+        caculaterDisplay.text = ""
+    }
+    
+    
+    @IBAction func buttonAnd(_ sender: Any)
+    {
+      temp =
+        Int(caculaterDisplay.text!)!
+        
+        caculaterDisplay.text = ""
+        flag = 1
+    }
+    
+    
+    @IBAction func buttonMinus(_ sender: Any) {
+        temp =
+            Int(caculaterDisplay.text!)!
+        
+        caculaterDisplay.text = ""
+        flag = 2
+        
+    }
+    
+    @IBAction func buttonMultiply(_ sender: Any) {
+        temp =
+            Int(caculaterDisplay.text!)!
+        
+        caculaterDisplay.text = ""
+        flag = 3
+        
+    }
+    
+    @IBAction func buttonDivided(_ sender: Any) {
+        temp =
+            Int(caculaterDisplay.text!)!
+        
+        caculaterDisplay.text = ""
+        flag = 4
+        
+    }
+    
+    @IBAction func button1(_ sender: Any)
+    {
+        caculaterDisplay.text = caculaterDisplay.text! + "1"
+    }
+    
+    
+    @IBAction func button2(_ sender: Any)
+    {
+        caculaterDisplay.text = caculaterDisplay.text! + "2"
+    }
+    
+    
+    @IBAction func button3(_ sender: Any)
+    {
+        caculaterDisplay.text = caculaterDisplay.text! + "3"
+    }
+    
+    
+    @IBAction func button4(_ sender: Any)
+    {
+        caculaterDisplay.text = caculaterDisplay.text! + "4"
+    }
+    
+    
+    @IBAction func button5(_ sender: Any)
+    {
+        caculaterDisplay.text = caculaterDisplay.text! + "5"
+    }
+    
+    
+    @IBAction func button6(_ sender: Any)
+    {
+        caculaterDisplay.text = caculaterDisplay.text! + "6"
+    }
+    
+    
+    @IBAction func button7(_ sender: Any)
+    {
+        caculaterDisplay.text = caculaterDisplay.text! + "7"
+    }
+    
+    
+    @IBAction func button8(_ sender: Any)
+    {
+        caculaterDisplay.text = caculaterDisplay.text! + "8"
+    }
+    
+    
+    @IBAction func button9(_ sender: Any)
+    {
+        caculaterDisplay.text = caculaterDisplay.text! + "9"
+    }
+    
+    
+    @IBAction func button0(_ sender: Any)
+    {
+        caculaterDisplay.text = caculaterDisplay.text! + "0"
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
